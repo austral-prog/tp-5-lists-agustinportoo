d83981e440c477e48870d0e5b0a88885eab02aae
agustinportoo <tanitoporto@gmail.com>
2026-04-07 11:46:00 -0300

    Update exercise_find_min.py

--- exercise_find_min.py
+++ exercise_find_min.py
@@ -11,4 +11,8 @@ def find_min(lista):
     Returns:
         El valor mínimo de la lista o None si está vacía
     """
-    pass  # Reemplazar con tu implementación
+    if lista!=[]:
+        minimo=min(lista)
+        return minimo
+    else:
+        return None
